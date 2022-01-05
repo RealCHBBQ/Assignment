3ac687087062fe9c42f339f3c62e3df6d842f7b9
Cheng Chun Hin <incrediblech1125@gmail.com>
2022-01-05 17:44:53 +0800

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by RealCH on 3/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
